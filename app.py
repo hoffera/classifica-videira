c75ee83fc7db30b058ed76d9e6ff944c446f4237
Felipe hoffmeister <81923101+hoffera@users.noreply.github.com>
2025-07-01 04:58:04 +0000

    commit

--- app.py
+++ app.py
@@ -52,12 +52,12 @@ def previsao(interpreter, imagem):
     # DEBUG: veja se o modelo está retornando alguma coisa
     st.write("Saída do modelo:", output_data)
 
-    classes = ['Aedes_aegypti_landing',
-    'Aedes_aegypti_smashed',
-    'Aedes_albopictus_landing',
-    'Aedes_albopictus_smashed',
-    'Culex_quinquefasciatus_landing',
-    'Culex_quinquefasciatus_smashed']
+    classes = ['Culex_landing',
+    'Culex_smashed',
+    'aegypti_landing',
+    'aegypti_smashed',
+    'albopictus_landing',
+    'albopictus_smashed']
     df = pd.DataFrame()
     df['classes'] = classes
     df['probabilidades (%)'] = 100 * output_data[0]
